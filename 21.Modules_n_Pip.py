558e435877c4592d05d5b470ef683e3387f0ae1e
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 03:23:18 +0530

    Twenty-first Lesson

---
+++ 21.Modules_n_Pip.py
@@ -0,0 +1,12 @@
+import Useful_tools
+
+#import docx  #installed python docx from chrome
+
+#(docx)
+
+print(Useful_tools.roll_dice(10))
+
+# Using Modules::
+# https://docs.python.org/3/py-modindex.html
+
+#Package Manager:  pip
