84c12de1d6ac17f99dbf1e45297c0f807e1de39f
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-14 23:28:16 +0530

    Second Lesson

---
+++ 2.Numbers.py
@@ -0,0 +1,25 @@
+from math import *   # for use after round function
+
+# Numbers
+print(-2.809)  # Output: -2.809  etc. we can have values printed
+print(3 + 4.5)  # Output: 7.5  etc. 4 main mathematical operations
+print(3 * 4 + 5)  # Output: 17  takes care of precedence
+print(3 * (4 + 5))  # Output: 27  takes care of precedence
+print(10 % 3)  # Output: 1  Modulus Operator
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " is cool! ")  # If you don't convert it to string, Python throws an error
+
+# Functions in Numbers
+
+my_num2 = -5.674
+print(abs(my_num2))  # Output: 5.674  Positive value
+print(pow(3, 3))  # Output: 27  power function
+print(max(4, 17))  # Output: 17  Maximum function (usually takes int comparison)
+print(min(4, 456))  # Output: 4  Minimum function (usually takes int comparison)
+print(round(my_num2))  # Output: -6  Round function
+
+print(floor(3.678))  # Output: 3 Floor value
+print(ceil(3.678))  # Output: 4 Ceiling value
+print(sqrt(49.49))  # Output: 7.034912934784623  Square root value
