dcf27c07544beb7d0a6d661ef6b9ff2df14c609b
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 03:23:43 +0530

    Twenty-second Lesson

---
+++ 22.app.py
@@ -0,0 +1,8 @@
+from 22.Classes_Objects import Student
+
+#Student1 is an object
+student1 = Student("Jim", "Business", 3.1, False)
+student1 = Student("Pam", "Art", 3.5, True)
+
+print(student1.gpa)
+print(student2.gpa)
