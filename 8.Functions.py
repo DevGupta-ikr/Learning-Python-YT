3831d6b4698efdd03d5670005a4fa86dd01c0ff2
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 00:23:43 +0530

    Eight Lesson

---
+++ 8.Functions.py
@@ -0,0 +1,14 @@
+
+# Function Declaration
+def say_hi():
+    print("Hello User")
+
+print("Top")
+say_hi()  # To execute the code
+print("Bottom")
+
+def say_name(name, age):
+    print("Hello " + name + ", you are " + age)
+
+
+say_name("Peter", "22")  # passing value into function
